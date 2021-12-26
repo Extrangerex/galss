65462515e527a4b579c12b5aba33e0f1df8ed722
Elliot <jopichardomartinez@gmail.com>
2021-12-26 15:51:25 -0400

    change

--- lib/blocs/login/login_bloc.dart
+++ lib/blocs/login/login_bloc.dart
@@ -3,5 +3,15 @@ import 'package:galss/blocs/login/login_event.dart';
 import 'package:galss/blocs/login/login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginState());
+  LoginBloc() : super(LoginState()) {
+    on<LoginUsernameChanged>((event, emit) => emit(state.copyWith(username: event.username)));
+    on<LoginPasswordChanged>((event, emit) => emit(state.copyWith(password: event.password)));
+    on<LoginFormSubmitted>(_onLoginFormSubmitted);
+  }
+
+  void _onLoginFormSubmitted(LoginFormSubmitted event, Emitter<LoginState> emit) {
+
+  }
+
+
 }
