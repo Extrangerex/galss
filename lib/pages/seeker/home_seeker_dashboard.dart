a39340cc67061cf9852f7a14a434bb044697dcd1
Elliot Pichardo <jopichardomartinez@gmail.com>
2022-01-31 21:16:08 -0400

    change

--- lib/pages/seeker/home_seeker_dashboard.dart
+++ lib/pages/seeker/home_seeker_dashboard.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:galss/blocs/home_seeker_catalog/home_seeker_catalog_bloc.dart';
+import 'package:galss/blocs/home_seeker_catalog/home_seeker_catalog_event.dart';
+import 'package:galss/blocs/home_seeker_catalog/home_seeker_catalog_state.dart';
 import 'package:galss/generated/l10n.dart';
 import 'package:galss/shared/icons_img.dart';
 import 'package:galss/shared/image_btn.dart';
 import 'package:galss/shared/imaged_background_container.dart';
+import 'package:galss/shared/recently_added_models.dart';
 
 class HomeSeekerDashboard extends StatefulWidget {
   const HomeSeekerDashboard({Key? key}) : super(key: key);
@@ -15,49 +20,75 @@ class _HomeSeekerDashboardState extends State<HomeSeekerDashboard> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      body: ImagedBackgroundContainer(
-          child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(bottom: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _btnIconLabel(
-                    child: ImageBtn(
-                        image:
-                            iconImg(customIconImg: CustomIconImg.icMiPerfil)),
-                    label: Text(S.current.my_profile)),
-                _btnIconLabel(
-                    child: ImageBtn(
-                        image:
-                            iconImg(customIconImg: CustomIconImg.icCercaDeMi)),
-                    label: Text(S.current.close_to_me))
-              ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (create) => HomeSeekerCatalogBloc()
+              ..add(const HomeSeekerGetRecentlyAddedModelsEvent())),
+      ],
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        body: ImagedBackgroundContainer(
+            child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _btnIconLabel(
+                      child: ImageBtn(
+                          image:
+                              iconImg(customIconImg: CustomIconImg.icMiPerfil)),
+                      label: Text(S.current.my_profile)),
+                  _btnIconLabel(
+                      child: ImageBtn(
+                          image: iconImg(
+                              customIconImg: CustomIconImg.icCercaDeMi)),
+                      label: Text(S.current.close_to_me))
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                _btnIconLabel(
-                    child: ImageBtn(
-                        image:
-                            iconImg(customIconImg: CustomIconImg.icCatalogo)),
-                    label: Text(S.current.model_catalog)),
-                _btnIconLabel(
-                    child: ImageBtn(
-                        image:
-                            iconImg(customIconImg: CustomIconImg.icConexiones)),
-                    label: Text(S.current.my_connections))
-              ],
+            Container(
+              margin: const EdgeInsets.only(bottom: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _btnIconLabel(
+                      child: ImageBtn(
+                          image:
+                              iconImg(customIconImg: CustomIconImg.icCatalogo)),
+                      label: Text(S.current.model_catalog)),
+                  _btnIconLabel(
+                      child: ImageBtn(
+                          image: iconImg(
+                              customIconImg: CustomIconImg.icConexiones)),
+                      label: Text(S.current.my_connections))
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+            BlocBuilder<HomeSeekerCatalogBloc, HomeSeekerCatalogState>(
+                builder: (context, state) {
+              if (state.models.isEmpty) {
+                return SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return Row(
+                children: state.models
+                    .map((e) => RecentlyAddedModelCard(
+                          model: e,
+                        ))
+                    .toList(),
+              );
+            })
+            // Flexible(flex: 1, child: SizedBox(height: 100,child: RecentlyAddedModels()))
+          ],
+        )),
+      ),
     );
   }
 
--- lib/shared/recently_added_models.dart
+++ lib/shared/recently_added_models.dart
@@ -1,12 +1,73 @@
-
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:galss/blocs/home_seeker_catalog/home_seeker_catalog_bloc.dart';
+import 'package:galss/blocs/home_seeker_catalog/home_seeker_catalog_event.dart';
+import 'package:galss/blocs/home_seeker_catalog/home_seeker_catalog_state.dart';
+import 'package:galss/models/user.dart';
+import 'package:galss/services/http_service.dart';
+import 'package:galss/shared/images.dart';
 
-class RecentlyAddedModels extends StatelessWidget {
+class RecentlyAddedModelCard extends StatelessWidget {
+  final User model;
+
+  const RecentlyAddedModelCard({Key? key, required this.model})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (create) => HomeSeekerCatalogBloc()
+                ..add(const HomeSeekerGetRecentlyAddedModelsEvent()))
+        ],
+        child: BlocBuilder<HomeSeekerCatalogBloc, HomeSeekerCatalogState>(
+            builder: (context, state) {
+          if (state.models.isEmpty) {
+            return Container();
+          }
+
+          return Row(
+            // children: [],
+            children: state.models.map((e) => modelCard(e)).toList(),
+          );
+        }));
+  }
+
+  Widget modelCard(User user) {
+    return SizedBox(
+      height: 180,
+      width: 180,
+      child: Stack(
+        children: [
+          Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: CachedNetworkImage(
+                imageUrl:
+                    "${HttpService.apiBaseUrl}/${user.profilePhoto?.urlPath}",
+                errorWidget: (context, url, error) =>
+                    Image(image: placeHolderProfileImg),
+              )),
+          Positioned(
+            child: ListTile(
+              title: Text("${user.model?.fullName}"),
+              subtitle: Column(
+                children: [
+                  Text("${user.currentLocation?.name}"),
+                  Text("${user.country?.name}"),
+                ],
+              ),
+              isThreeLine: true,
+            ),
+            bottom: 0,
+          )
+        ],
+      ),
+    );
   }
 }
-
